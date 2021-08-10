f62a58903066125c0405a4e172a3f7b0b289f9af
David <caico@fibertel.com.ar>
2021-08-10 19:23:13 -0300

    Creacion de la funcion obtenerSecuencia que devuelve la secuencia de acuerdo a la frase ingresada. - Ordenamiento y limpieza del codigo.

--- Program.cs
+++ Program.cs
@@ -16,22 +16,21 @@ namespace desafio_consola
             Console.WriteLine("O presione enter para terminar");
             string mensajeCadena;
             mensajeCadena = Console.ReadLine();
+            string mensajeSecuencia = "";
 
             while(mensajeCadena != ""){
 
                 List<string>  lista = validarMensaje(mensajeCadena); 
 
                 if( lista != null){
-
-                    Console.WriteLine("Cadena válida: " + lista.Count);
-                    foreach(string c in lista){
-                        Console.WriteLine("Palabra: " + c + " - ");
-                    }
+                    //Obtengo la secuencia correspondinte.
+                    mensajeSecuencia = obtenerSecuencia(lista);     
+                    Console.WriteLine("Secuencia obtenida: " + mensajeSecuencia);               
                 }else{
                     Console.WriteLine("Cadena inválida");
                 }
 
-                contador = contador++;
+                contador++;
                 Console.WriteLine("Ingrese una palabra o frase nro: " + contador);
                 Console.WriteLine("O presione enter para terminar");              
                 mensajeCadena = Console.ReadLine();
@@ -55,23 +54,163 @@ namespace desafio_consola
         // Si la cadena no es vacía o sólo espacios, devuelve una lista con las palabras de la misma,
         // caso contrario devuelvee una lista con valor null.        
         public static List<string> validarMensaje(string mensajeCadena){
-
+          
             // Caracteres aceptados en la cadena
             string expresion = @"^[a-zA-Z\s]*$";
             Regex r = new Regex(expresion);
             List<string> listaCadena = null;            
            
             if(r.IsMatch(mensajeCadena)){ 
-
+                
                 mensajeCadena = mensajeCadena.TrimStart(' ');
                 mensajeCadena = mensajeCadena.TrimEnd(' ');                
                 Regex espacio = new Regex(@"\s+"); 
-                string cadena = espacio.Replace(mensajeCadena,"-");
-                Console.WriteLine("Cadena: " + cadena);                
-                listaCadena = cadena.Split('-').ToList();              
+                string cadena = espacio.Replace(mensajeCadena,"-");                             
+                listaCadena = cadena.Split('-').ToList(); 
             }
 
             return listaCadena;
+        }
+
+        //Devuelve un string con la secuencia de acuerdo a las cadenas contenidas en la lista.
+        public static string obtenerSecuencia(List<string> listaCadena){
+            
+            string cadenaSalida = "";        
+            
+            foreach(string cadena in listaCadena){
+               
+                String cadenaPalabra = "";
+                string numeroAnterior = "0";
+                string numero = "";
+                foreach(char caracter in cadena.ToLower()){
+                    string cadenaCaracter = "";
+                    switch (caracter){
+                        case 'a': 
+                            cadenaCaracter = "2";
+                            numero = "2";
+                            break;
+                        case 'b': 
+                            cadenaCaracter = "22";
+                            numero = "2";
+                            break;
+                        case 'c': 
+                            cadenaCaracter = "222";
+                            numero = "2";
+                            break;
+                        case 'd': 
+                            cadenaCaracter = "3";
+                            numero = "3";
+                            break;
+                        case 'e': 
+                            cadenaCaracter = "33";
+                            numero = "3";
+                            break;
+                        case 'f': 
+                            cadenaCaracter = "333";
+                            numero = "3";
+                            break;
+                        case 'g': 
+                            cadenaCaracter = "4";
+                            numero = "4";
+                            break;
+                        case 'h': 
+                            cadenaCaracter = "44";
+                            numero = "4";
+                            break;
+                        case 'i': 
+                            cadenaCaracter = "444";
+                                numero = "4";
+                            break;
+                        case 'j': 
+                            cadenaCaracter = "5";
+                            numero = "5";
+                            break;
+                        case 'k': 
+                            cadenaCaracter = "55";
+                            numero = "5";
+                            break;
+                        case 'l': 
+                            cadenaCaracter = "555";
+                            numero = "5";
+                            break;
+                        case 'm': 
+                            cadenaCaracter = "6";
+                            numero = "6";
+                            break;
+                        case 'n': 
+                            cadenaCaracter = "66";
+                            numero = "6";
+                            break;
+                        case 'o': 
+                            cadenaCaracter = "666";
+                            numero = "6";
+                            break;
+                        case 'p': 
+                            cadenaCaracter = "7";
+                            numero = "7";
+                            break;
+                        case 'q': 
+                            cadenaCaracter = "77";
+                            numero = "7";
+                            break;
+                        case 'r': 
+                            cadenaCaracter = "777";
+                            numero = "7";
+                            break;
+                        case 's': 
+                            cadenaCaracter = "7777";
+                            numero = "7";
+                            break;
+                        case 't': 
+                            cadenaCaracter = "8";
+                            numero = "8";
+                            break;
+                        case 'u': 
+                            cadenaCaracter = "88";
+                            numero = "8";
+                            break;
+                        case 'v': 
+                            cadenaCaracter = "888";
+                            numero = "8";
+                            break;
+                        case 'w': 
+                            cadenaCaracter = "9";
+                            numero = "9";
+                            break;
+                        case 'x': 
+                            cadenaCaracter = "99";
+                            numero = "9";
+                            break;
+                        case 'y': 
+                            cadenaCaracter = "999";
+                            numero = "9";
+                            break;
+                        case 'z': 
+                            cadenaCaracter = "9999";
+                            numero = "9";
+                            break; 
+                    }
+
+                    //Verifica si corresponde a la misma tecla
+                    if(numero == numeroAnterior){
+                        cadenaPalabra = cadenaPalabra + " ";
+                    }
+                    cadenaPalabra = cadenaPalabra + cadenaCaracter;
+                    numeroAnterior = numero;
+                    
+                }                        
+                
+                //Si es la primer palabra, no grega espacio en la cadena de salida
+                if(cadenaSalida == ""){
+                    cadenaSalida = cadenaSalida + cadenaPalabra;
+                }else{
+                    cadenaSalida = cadenaSalida + "0" + cadenaPalabra;
+                }
+
+
+            }
+            
+            return cadenaSalida;
         }
     }
 }
